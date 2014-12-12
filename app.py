0af1beda378fd0e44fc7c0675dadc7797174f8a4
yuanluya <yuanluya@umich.edu>
2014-12-11 20:08:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,6 @@ from flask import render_template
 
 schedule_api_consumer_key = '0xQinezMClN8jkIZaXWoKg4ZfZca'
 schedule_api_secret_key   = 'w6QBOo3QCbfRhtSsfDmiXNzFgVsa'
-<<<<<<< HEAD
 Schedule_api_consumer_key = 'B18ATlny4c_pO04Oc26mfBdM6AIa'
 Schedule_api_secret_key   = 'yohm_cnTKUV5RGSNBNEfKMSljYEa' 
-=======
->>>>>>> origin/master
 app = Flask(__name__)
